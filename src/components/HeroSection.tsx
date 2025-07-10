defc68c1ffc93dadfe451938941fa75d15256977
Saksham-Shukla-05 <get467997@gmail.com>
2025-07-10 22:48:17 +0530

    Make changes in Hero section add an Image

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -8,27 +8,41 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import Image from "next/image";
 
 export default function CardDemo() {
   return (
-    // understand why is it getting space from left and right
     <Card className="w-full max-w-7xl rounded-none border-none p-0">
       <CardContent className="p-0">
-        <div className="flex flex-row flex-wrap  p-2   ">
-          <div className="md:w-1/2 w-full flex gap-5  flex-col ">
-            <h1 className="md:text-4xl text-2xl">
+        <div className="flex flex-row gap-8 md:gap-0 flex-wrap  p-2   ">
+          <div className="md:w-1/2 w-full flex justify-evenly md:items-start items-center md:gap-0 gap-6  p-1 flex-col ">
+            <h1 className="md:text-3xl text-2xl">
               Hi, I&apos;m Saksham Shukla 👋
             </h1>
-            <p>A passionate Full Stack Developer from India </p>
-            <p>
+            <p className="text-center">
+              A passionate Full Stack Developer from India{" "}
+            </p>
+            <p className="text-center">
               I specialize in building modern, scalable web apps with MERN stack
               and TypeScript.
             </p>
+            <div className="items-start w-fit  flex gap-3 ">
+              <Button className="bg-purple-600 hover:bg-purple-700 cursor-pointer text-white">
+                Download Resume
+              </Button>
+              <Button className="bg-purple-600 hover:bg-purple-700 cursor-pointer text-white">
+                Contact Me
+              </Button>
+            </div>
           </div>
-          <div className="md:w-1/2 w-full gap-2 flex  justify-center ">
-            Card
+          <div className="md:w-1/2 w-full gap-2 justify-center flex p-1 ">
+            <Image
+              className="rounded-2xl "
+              src={"/Passport Size Photo.jpg"}
+              alt="saksham"
+              width={300}
+              height={300}
+            />
           </div>
         </div>
       </CardContent>
