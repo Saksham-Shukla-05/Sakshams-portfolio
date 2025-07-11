2c3a1e969eff7e7c7074ea5d34a5039fe7733712
Saksham-Shukla-05 <get467997@gmail.com>
2025-07-11 22:29:22 +0530

    Add Avatars in the hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
-import { AppSidebar } from "@/components/app-sidebar";
-import { SidebarProvider } from "@/components/ui/sidebar";
 import Navbar from "@/components/Navbar";
 import HeroSection from "@/components/HeroSection";
 export default function Home() {
   return (
-    <div className="flex flex-col w-full h-fit  items-center gap-2 ">
+    <div className="flex flex-col w-full h-fit md:p-4 p-2 gap-5 items-center  ">
       {" "}
-      <nav className="w-full h-fit  p-2   flex  justify-between items-center">
+      <nav className="w-full h-fit    flex  justify-between items-center">
         <div className="font-mono md:text-2xl  text-[15px]  dark:text-purple-500 font-extrabold">
           &lt;/Saksham&gt;
         </div>
--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -12,17 +12,17 @@ import Image from "next/image";
 
 export default function CardDemo() {
   return (
-    <Card className="w-full max-w-7xl rounded-none border-none p-0">
+    <Card className="dark:bg-black bg-white w-full max-w-7xl rounded-none border-none p-0">
       <CardContent className="p-0">
         <div className="flex flex-row gap-8 md:gap-0 flex-wrap  p-2   ">
           <div className="md:w-1/2 w-full flex justify-evenly md:items-start items-center md:gap-0 gap-6  p-1 flex-col ">
-            <h1 className="md:text-3xl text-2xl">
-              Hi, I&apos;m Saksham Shukla 👋
+            <h1 className="lg:text-3xl md:text-2xl text-xl -mb-2 md:font-extrabold font-normal">
+              Hi, I&apos;m Saksham Shukla👋
             </h1>
-            <p className="text-center">
+            <p className="text-center md:text-lg text-sm md:text-left font-semibold">
               A passionate Full Stack Developer from India{" "}
             </p>
-            <p className="text-center">
+            <p className="text-center md:text-left">
               I specialize in building modern, scalable web apps with MERN stack
               and TypeScript.
             </p>
@@ -35,13 +35,13 @@ export default function CardDemo() {
               </Button>
             </div>
           </div>
-          <div className="md:w-1/2 w-full gap-2 justify-center flex p-1 ">
+          <div className="md:w-1/2 justify-center w-full gap-2 flex p-1 ">
             <Image
-              className="rounded-2xl "
-              src={"/Passport Size Photo.jpg"}
+              className="rounded-2xl mix-blend-soft-light"
+              src={"/avataaars (1).png"}
               alt="saksham"
-              width={300}
-              height={300}
+              width={280}
+              height={170}
             />
           </div>
         </div>
