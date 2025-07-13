e7b43bde139e44a7432747f810845f4c714cde8e
Saksham-Shukla-05 <get467997@gmail.com>
2025-07-13 23:24:35 +0530

    Make some UI changes and added tailwind classes

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -4,11 +4,11 @@ import Image from "next/image";
 
 export default function CardDemo() {
   return (
-    <Card className="dark:bg-black bg-white w-full rounded-none border-none shadow-none  p-0">
-      <CardContent className="p-0 md:bg-none bg-[url('/wave-haikei3.png')] bg-cover bg-bottom   ">
-        <div className="flex z-20 flex-col md:flex-row items-center justify-between gap-10 md:gap-0 ">
+    <Card className="md:mt-0 mt-20 h-screen dark:bg-black bg-white w-full rounded-none border-none shadow-none  p-0">
+      <CardContent className="h-full   p-0 md:bg-none bg-[url('/wave-haikei3.png')] bg-cover bg-top   ">
+        <div className="h-full flex  flex-col md:flex-row items-center  justify-between gap-10 md:gap-0 ">
           {/* Left Section */}
-          <div className="w-full md:w-1/2 flex p-1 flex-col gap-8 text-center md:text-left items-center md:items-start">
+          <div className="w-full h-full justify-center gap-10 md:w-1/2 flex p-1 flex-col   text-center md:text-left  items-center md:items-start">
             <h1 className="text-2xl sm:text-3xl lg:text-4xl font-bold">
               Hi, I&apos;m Saksham Shukla
             </h1>
@@ -21,24 +21,21 @@ export default function CardDemo() {
             </p>
 
             <div className="flex flex-col sm:flex-row gap-3 mt-4 w-full sm:w-auto justify-center md:justify-start">
-              <Button className="bg-purple-700  text-white w-full sm:w-auto">
+              <Button className="bg-purple-700 cursor-pointer  text-white w-full sm:w-auto">
                 Download Resume
               </Button>
-              <Button className="bg-purple-700 text-white w-full sm:w-auto">
+              <Button className="bg-purple-700 cursor-pointer text-white w-full sm:w-auto">
                 Contact Me
               </Button>
             </div>
           </div>
 
           {/* Right Section */}
-          <div className="w-full md:w-1/2 flex justify-center">
-            <Image
-              className="rounded-2xl w-48 sm:w-52 md:w-56 lg:w-72 xl:w-80 h-auto"
+          <div className="w-full md:w-1/2   flex justify-around">
+            <img
+              className="rounded-2xl w-48  sm:w-52  md:w-56 lg:w-72 xl:w-80 h-auto"
               src="/avataaars (2).png"
               alt="saksham"
-              width={280}
-              height={170}
-              priority
             />
           </div>
         </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -16,66 +16,76 @@ export default function NavigationMenuDemo() {
   const pathname = usePathname();
   const isActive = (href: string) => pathname === href;
   return (
-    <NavigationMenu viewport={false}>
-      <NavigationMenuList className="gap-5  px-4 hidden md:flex">
-        <NavigationMenuItem>
-          <NavigationMenuLink
-            className={
-              isActive("/")
-                ? "md:text-lg text-sm text-purple-500 dark:text-purple-500 font-semibold"
-                : "md:text-lg text-sm text-black dark:text-white"
-            }
-          >
-            <Link href={"/"}>Home</Link>
-          </NavigationMenuLink>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <NavigationMenuLink
-            className={
-              isActive("/About")
-                ? " md:text-lg text-sm  text-purple-500 dark:text-purple-500 font-semibold"
-                : " md:text-lg text-sm  text-black dark:text-white"
-            }
-          >
-            <Link href={"/About"}>About</Link>
-          </NavigationMenuLink>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <NavigationMenuLink
-            className={
-              isActive("/Projects")
-                ? "md:text-lg text-sm   text-purple-500 dark:text-purple-500 font-semibold"
-                : "md:text-lg text-sm   text-black dark:text-white"
-            }
-          >
-            <Link href="/Projects">Projects</Link>
-          </NavigationMenuLink>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <NavigationMenuLink
-            className={
-              isActive("/Contact")
-                ? "md:text-lg text-sm text-purple-500 dark:text-purple-500 font-semibold"
-                : "md:text-lg text-sm   text-black dark:text-white"
-            }
-          >
-            <Link href={"/Contact"}>Contact</Link>
-          </NavigationMenuLink>
-        </NavigationMenuItem>
+    <nav className="w-full h-fit  mb-5 md:mb-0  fixed flex  justify-between items-center">
+      <div className="font-mono md:text-2xl text-xl   dark:text-purple-500 font-extrabold">
+        &lt;/Saksham&gt;
+      </div>
+      <NavigationMenu viewport={false}>
+        <NavigationMenuList className="gap-5  px-4 hidden md:flex ">
+          <NavigationMenuItem>
+            <NavigationMenuLink
+              className={
+                isActive("/")
+                  ? "md:text-lg text-sm text-purple-500 dark:text-purple-500 font-semibold"
+                  : "md:text-lg text-sm text-black dark:text-white"
+              }
+            >
+              <Link href={"/"}>Home</Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
 
-        <NavigationMenuItem>
-          <NavigationMenuLink className="md:text-lg text-sm">
-            <Link href={"/Resume"}>Resume</Link>
-          </NavigationMenuLink>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuLink
+              className={
+                isActive("/About")
+                  ? " md:text-lg text-sm  text-purple-500 dark:text-purple-500 font-semibold"
+                  : " md:text-lg text-sm  text-black dark:text-white"
+              }
+            >
+              <Link href={"/About"}>About</Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+
+          <NavigationMenuItem>
+            <NavigationMenuLink
+              className={
+                isActive("/Projects")
+                  ? "md:text-lg text-sm   text-purple-500 dark:text-purple-500 font-semibold"
+                  : "md:text-lg text-sm   text-black dark:text-white"
+              }
+            >
+              <Link href="/Projects">Projects</Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+
+          <NavigationMenuItem>
+            <NavigationMenuLink
+              className={
+                isActive("/Contact")
+                  ? "md:text-lg text-sm text-purple-500 dark:text-purple-500 font-semibold"
+                  : "md:text-lg text-sm   text-black dark:text-white"
+              }
+            >
+              <Link href={"/Contact"}>Contact</Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+
+          <NavigationMenuItem>
+            <NavigationMenuLink className="md:text-lg text-sm">
+              <Link href={"/Resume"}>Resume</Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+
+          <NavigationMenuItem>
+            <DarkMode />
+          </NavigationMenuItem>
+        </NavigationMenuList>
+
+        <NavigationMenuList className="md:hidden   flex gap-2 justify-between items-center">
           <DarkMode />
-        </NavigationMenuItem>
-      </NavigationMenuList>
-      <NavigationMenuList className="md:hidden   flex gap-2 justify-between items-center">
-        <DarkMode />
-        <SidebarTrigger />
-      </NavigationMenuList>
-    </NavigationMenu>
+          <SidebarTrigger />
+        </NavigationMenuList>
+      </NavigationMenu>
+    </nav>
   );
 }
