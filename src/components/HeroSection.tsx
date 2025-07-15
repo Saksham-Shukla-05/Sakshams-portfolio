5c8ba46341675436890219f1eeecc534825d1f27
Saksham-Shukla-05 <get467997@gmail.com>
2025-07-15 23:54:37 +0530

    Change Hero Section UI to display the image in different viewports

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -5,11 +5,11 @@ import Image from "next/image";
 export default function CardDemo() {
   return (
     <Card className="md:h-screen  border-none dark:bg-black bg-white  w-full rounded-none shadow-none  p-5">
-      <CardContent className="h-full relative  border-none md:bg-[url('/blob-scene-haikei2.png')] p-0 bg-[url('/wave-haikei3.png')] bg-cover bg-no-repeat bg-bottom   ">
+      <CardContent className="h-full relative  border-none md:bg-[url('/blob-blob-2.png')] p-0 bg-[url('/wave-haikei3.png')] bg-cover bg-no-repeat bg-bottom   ">
         <div className="h-full flex flex-col md:flex-row items-center  justify-between gap-10 md:gap-0 ">
           {/* Left Section */}
           <div className="w-full h-full justify-center gap-8  flex flex-col text-center items-center ">
-            <h1 className="text-2xl sm:text-3xl lg:text-5xl font-bold">
+            <h1 className="md:text-5xl text-2xl font-bold">
               Hi, I&apos;m Saksham Shukla
             </h1>
             <p className="text-base sm:text-lg font-semibold">
