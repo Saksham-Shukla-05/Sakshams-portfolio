9c4a0e506c9ec6856ebe1b4cf34994aa9cf400c4
Saksham-Shukla-05 <get467997@gmail.com>
2025-07-20 22:43:17 +0530

    Add new styling into the page and the
    Add new items into the glimse component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import HeroSection from "@/components/HeroSection";
 import Glimse from "@/components/Glimse";
 export default function Home() {
   return (
-    <main className=" dark:bg-black bg-white flex flex-col w-full  md:gap-0 gap-12 items-center  ">
+    <main className=" dark:bg-black bg-white flex flex-col w-full  md:gap-0 gap-5 items-center  ">
       <Navbar />
       <HeroSection />
       <Glimse />
