35a7ec661dcb33e52d13f02d26ef66888624ae4b
Saksham-Shukla-05 <get467997@gmail.com>
2025-07-24 23:14:39 +0530

    Change the config and Add style in the components

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,11 +8,11 @@ export default function Footer() {
   };
 
   return (
-    <footer className="p-4 mt-20 border-t w-full flex flex-col gap-12 bg-gray-200 dark:bg-black dark:text-gray-300 relative">
+    <footer className="p-5 mt-20 border-t w-full flex flex-col gap-12 bg-gray-200 dark:bg-black dark:text-gray-300 relative">
       <div className="flex flex-col mt-10 md:flex-row justify-between items-center gap-12">
         {/* Summary */}
-        <div className="text-center md:border-r-4 px-4 border-none md:w-1/2 w-full md:text-justify max-w-md">
-          <h3 className="text-xl font-bold text-black dark:text-white">
+        <div className="text-center md:border-r-4 px-4 border-none md:w-1/2 w-full lg:text-justify max-w-md">
+          <h3 className="text-xl font-mono font-bold text-purple-700 dark:text-white">
             &lt;/Saksham&gt;
           </h3>
           <p className="text-sm text-gray-600 dark:text-gray-400 mt-2 mb-2">
@@ -27,10 +27,10 @@ export default function Footer() {
         </div>
 
         {/* Links Section */}
-        <div className="flex flex-row flex-wrap items-start gap-12 md:w-1/2 w-full justify-around">
+        <div className="flex flex-row flex-wrap  items-start gap-12 lg:w-1/2 w-full justify-around">
           {/* Socials */}
-          <div className="flex text-center items-center flex-col gap-5">
-            <p className="font-medium">Socials</p>
+          <div className="flex text-center mt-0 items-center flex-col gap-9">
+            <p className="font-medium">Let's connect</p>
             <Link
               href="https://github.com/Saksham-Shukla-05"
               target="_blank"
