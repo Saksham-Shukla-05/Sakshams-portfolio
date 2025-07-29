aeb31b933433a428fd7ce24a7d574cf50b7e2b72
Saksham-Shukla-05 <get467997@gmail.com>
2025-07-29 23:20:23 +0530

    change the layout of the about page

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -1,27 +1,24 @@
 import { FaNode } from "react-icons/fa";
 const About = () => {
   return (
-    <section className="w-full min-h-screen px-6 justify-between  md:px-8 py-10 md:py-20 flex flex-col items-center gap-14">
+    <section className="w-full min-h-screen   justify-between   px-8 py-10 md:py-20 flex flex-col items-center gap-14">
       <div className="w-full    flex flex-col items-center gap-4 md:gap-6 text-center">
         <h2 className="text-3xl  sm:text-4xl md:text-5xl font-bold md:leading-loose leading-normal">
           Meet the <span className="text-purple-500">Developer</span>
         </h2>
         <div className="w-12 md:w-16 h-1 bg-purple-500 rounded-full" />
 
-        <p className="text-gray-700 dark:text-gray-300 text-sm md:text-base md:max-w-5xl  leading-relaxed">
-          I'm{" "}
+        <p className="text-gray-700 dark:text-gray-300 text-sm md:text-base md:max-w-5xl md:text-center text-justify leading-relaxed">
+          Hi, I'm{" "}
           <span className="font-semibold text-purple-500">Saksham Shukla</span>,
-          a full-stack developer passionate about creating fast, scalable, and
-          user-friendly web experiences. I primarily work with the{" "}
-          <span className="font-semibold">MERN stack</span> and{" "}
-          <span className="font-semibold">Next.js</span>, always eager to learn
-          more.
-        </p>
-
-        <p className="text-gray-700 dark:text-gray-300 text-sm md:text-base w-full md:max-w-3xl leading-relaxed">
-          From sleek user interfaces to robust backend systems, I turn ideas
-          into performant, real-world solutions — with clean code and keen
-          attention to detail.
+          a passionate and self-driven web developer currently in my final year
+          of the Integrated Master of Computer Applications (I-MCA) program at
+          Institute of Integrated Management Studies (IIPS), DAVV, Indore. I
+          specialize in full-stack web development with a focus on modern
+          technologies like React, Node.js, and Tailwind CSS. My academic
+          background, combined with hands-on project experience, reflects my
+          dedication to building clean, efficient, and user-friendly web
+          applications.
         </p>
       </div>
 
