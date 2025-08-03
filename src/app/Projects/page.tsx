320f2df290120b5f19f0fefc7ed77f546de08366
Saksham-Shukla-05 <get467997@gmail.com>
2025-08-03 19:44:19 +0530

    Add a under construction message on the project page

--- src/app/Projects/page.tsx
+++ src/app/Projects/page.tsx
@@ -1,8 +1,25 @@
 "use client";
+import Link from "next/link";
 import React from "react";
 
 function page() {
-  return <div>page</div>;
+  return (
+    <div className="md:p-8 p-4 flex flex-col items-center gap-5 justify-center border-b md:mt-4 mt-8  dark:bg-black bg-white w-full rounded-none shadow-none  ">
+      <p>Working on it!!</p>
+      <div className="flex items-center gap-3">
+        <p>Have a question? </p>
+
+        <p>
+          <Link
+            className="underline decoration-purple-500"
+            href="mailto:shuklasaksham729@gmail.com"
+          >
+            Email me.
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default page;
--- src/app/api/get-projects/route.ts
+++ src/app/api/get-projects/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 
-export async function POST(request: Request) {
+export async function GET(request: Request) {
   // Your logic here
   return NextResponse.json({ message: "Project added!", request });
 }
