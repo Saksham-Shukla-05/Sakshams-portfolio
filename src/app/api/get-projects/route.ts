a1af3bbcf103b99875032097fd9775038a544b0c
Saksham-Shukla-05 <get467997@gmail.com>
2025-08-04 22:02:47 +0530

    Refactor the get project route and Add a .env.example file to store  the record of the environment variables
    Rename the project model to Project.Model.ts

--- src/app/api/get-projects/route.ts
+++ src/app/api/get-projects/route.ts
@@ -1,6 +1,17 @@
+import dbConnect from "@/lib/dbConnect";
+import { Project } from "@/model/project.Model";
 import { NextResponse } from "next/server";
 
-export async function GET(request: Request) {
-  // Your logic here
-  return NextResponse.json({ message: "Project added!", request });
+export async function GET() {
+  try {
+    await dbConnect();
+
+    const projects = await Project.find({});
+    return NextResponse.json({ projects });
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Error while fetching projects", error },
+      { status: 500 }
+    );
+  }
 }
