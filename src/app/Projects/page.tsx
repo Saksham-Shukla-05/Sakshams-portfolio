193e0f74639ec0449c92870c2a8ac762ea112ab4
Saksham-Shukla-05 <get467997@gmail.com>
2025-08-06 23:54:38 +0530

    refactor: Meassage appearing on the project section
    feat : add-project folder to add the projects via from

--- src/app/Projects/page.tsx
+++ src/app/Projects/page.tsx
@@ -6,16 +6,16 @@ import React from "react";
 function page() {
   return (
     <div className="md:p-8 p-4 flex flex-col items-center gap-5 justify-center   md:mt-4 mt-8  dark:bg-black bg-white w-full rounded-none shadow-none  ">
-      <p>Working on it!!</p>
-      <div className="flex items-center gap-3">
-        <p>Have a question? </p>
+      <p className="uppercase">under construction!!</p>
+      <div className="flex items-center gap-2">
+        <p>You can visit my github to see all projects I made! </p>
 
         <p>
           <Link
             className="underline decoration-purple-500"
-            href="mailto:shuklasaksham729@gmail.com"
+            href="https://github.com/Saksham-Shukla-05"
           >
-            Email me
+            GitHub
           </Link>
         </p>
       </div>
