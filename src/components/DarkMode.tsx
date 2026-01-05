0307a397ad5c95ccacd8a38ed68a92abd0456cb4
Saksham-Shukla-05 <get467997@gmail.com>
2026-01-05 20:08:37 +0530

    add new css to buttons

--- src/components/DarkMode.tsx
+++ src/components/DarkMode.tsx
@@ -11,13 +11,13 @@ export default function ModeToggle() {
 
   return (
     <Button
-      className="border-none p-3"
+      className="border-none p-3 cursor-pointer"
       variant="outline"
       size="icon"
       onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
     >
-      <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-      <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+      <Sun className="h-[1.2rem]   w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+      <Moon className="absolute   h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
     </Button>
   );
 }
